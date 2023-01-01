b185519a1c6deff7d330863dee70922202628965
MrWurste <wadid1199@gmail.com>
2023-01-01 14:02:16 +0100

    Initial commit

---
+++ src/main/java/com/dawid_kielbasa/liase_and_direct/LiaseAndDirectApplication.java
@@ -0,0 +1,13 @@
+package com.dawid_kielbasa.liase_and_direct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiaseAndDirectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiaseAndDirectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dawid_kielbasa/liase_and_direct/LiaseAndDirectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dawid_kielbasa.liase_and_direct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiaseAndDirectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
